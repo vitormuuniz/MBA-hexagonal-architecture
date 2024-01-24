ae767cc2155e02ab9a93f76f57746584f814da9b
Vitor Muniz <vitor.muniz@sensedia.com>
2024-01-24 17:31:47 -0300

    refactoring domain models

--- src/main/java/br/com/fullcycle/hexagonal/application/domain/Partner.java
+++ src/main/java/br/com/fullcycle/hexagonal/application/domain/Partner.java
@@ -2,14 +2,52 @@ package br.com.fullcycle.hexagonal.application.domain;
 
 import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
 
-public record Partner(PartnerId partnerId, Name name, Cnpj cnpj, Email email) {
-    public Partner {
+public class Partner {
+
+    private final PartnerId partnerId;
+    private Name name;
+    private Cnpj cnpj;
+    private Email email;
+
+    public Partner(final PartnerId partnerId, final String name, final String cnpj, final String email) {
         if (partnerId == null) {
             throw new ValidationException("Invalid partnerId for Partner");
         }
+        this.partnerId = partnerId;
+        this.setName(name);
+        this.setCnpj(cnpj);
+        this.setEmail(email);
     }
 
     public static Partner newPartner(String name, String cnpj, String email) {
-        return new Partner(PartnerId.unique(), new Name(name), new Cnpj(cnpj), new Email(email));
+        return new Partner(PartnerId.unique(), name, cnpj, email);
+    }
+
+    public PartnerId partnerId() {
+        return this.partnerId;
+    }
+
+    public Name name() {
+        return this.name;
+    }
+
+    public Cnpj cnpj() {
+        return this.cnpj;
+    }
+
+    public Email email() {
+        return this.email;
+    }
+
+    private void setName(final String name) {
+        this.name = new Name(name);
+    }
+
+    private void setCnpj(final String cnpj) {
+        this.cnpj = new Cnpj(cnpj);
+    }
+
+    private void setEmail(final String email) {
+        this.email = new Email(email);
     }
 }
